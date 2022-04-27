3a1a097e7d4f95900ddbdabf4ac17be7bcdea0ee
shadowmelds <hujincan15369@Gmail.com>
2022-04-28 01:31:18 +0800

    commit

--- src/global/base.ts
+++ src/global/base.ts
@@ -5,4 +5,20 @@ init();
 function init() {
     let jumpIcons = new JumpIconsComponent();
     jumpIcons.onInit()
+
+
+    const mobileBtn = document.getElementById('mobile-cta')
+    let tabs = document.querySelector('.tabs')
+    let nav = document.querySelector('.mobile-menu')
+
+    mobileBtn.addEventListener('click', () => {
+        // 添加 class="menu-btn" 到 nav
+        if (nav.className.indexOf('menu-btn-exit') > -1) {
+            tabs.classList.remove('menu-btn-exit');
+            nav.classList.remove('menu-btn-exit');
+        } else {
+            tabs.classList.add('menu-btn-exit');
+            nav.classList.add('menu-btn-exit');
+        }
+    })
 }
