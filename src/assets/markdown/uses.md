b3806ebe05240c7534f9a1143f72f6db9b578ff9
shadowmelds <hujincan15369@Gmail.com>
2022-05-08 22:20:40 +0800

    update

--- src/assets/markdown/uses.md
+++ src/assets/markdown/uses.md
@@ -1,10 +1,12 @@
 # 我使用了哪些工具？
 
+>更新于2022年5月8日
+
 这个页面的灵感来自在 [uses.tech](https://uses.tech/) 上介绍自己使用的工具。
 
 ### 🌐 网站
 
-该网站使用[Angular](https://angular.io/)构建，并部署在[Github pages](https://pages.github.com/)上。
+该网站最初用[Angular](https://angular.io/)构建，现在使用HTML、TS、SASS构建，并部署在[Github pages](https://pages.github.com/)上。
 
 ### 📑 系统+编辑器
 我个人用的是Windows 11 系统。并使用Windows Terminal作为终端应用。
@@ -14,21 +16,22 @@
 
 ### 💻 硬件
 
-- PC：`机械大师 C28` + `MAG B550M` + `Ryzen 5600X` + `Radeon RX550` + `海盗船 8/3200 * 2` + `WD BLUE SN550 500G 、1T`
-- Pixel 5 (鼠尾草色)
-- RAZER 曼巴眼镜蛇无线版
+- PC：`机械大师 C28` + `MAG B460M` + `Intel I7-10700` + `Radeon RX570` + `玄冰400组合泽诺风扇` + `海盗船 8/3200 * 2` + `WD BLUE SN550 500G 、1T`
+- Google Pixel 5 (鼠尾草色)
+- RAZER 曼巴眼镜蛇无线版鼠标
 - 斐尔可圣手二代双模 87键 (绿色+白色)
 - Beats urBeats3 耳机、 Razer Hammerhead TWS X 耳机
 - 定制守望先锋月神天使、粉红天使鼠标垫
 - Wacom CTL472
 - 小米屏幕挂灯
-- LG LM238WR-SPE1 定制4K显示器
+- LG LM238WR-SPE1 定制23.8英寸4K显示器
+- 小米快速液晶显示器24.5英寸 240Hz显示器
+- 猫王音响原子唱机B612蓝牙音箱
 
 ### ☕ 周边
 
 - 守望先锋雪球杯子
 - 守望先锋锌合金纪念币
-- 止白·咖啡分享壶、滤杯
-- Pixel 3XL（黑色）
+- 完美主义-止白·咖啡分享壶、滤杯
 - 台电 C40Pro 40000mAh 充电宝
 - SanDisk Extreme Pro USB 128G U盘
--- src/data/markdowns.ts
+++ src/data/markdowns.ts
@@ -5,7 +5,7 @@ export const MARKDOWNS_ALL: Markdown[] = [
   { id: -1, url: '/src/assets/markdown/shadowmeld_info.md', image: '/src/assets/img/shadowmeld.png', title: '个人介绍',
     content: '所有关于我的个人介绍',
     date: '2021.11.15', timeline: 2021},
-  { id: 1, url: '/src/assets/markdown/Uses.md', image: '/src/assets/photos/012.jpg', title: '我使用了哪些工具？',
+  { id: 1, url: '/src/assets/markdown/uses.md', image: '/src/assets/photos/012.jpg', title: '我使用了哪些工具？',
     content: '了解我使用哪些软件和设备',
     date: '2020.8.31', timeline: 2020},
   { id: 2, url: '/src/assets/markdown/fix_node_sass_install.md', image: '/src/assets/markdown/images/2021-12-30_082722.png',
@@ -24,13 +24,13 @@ export const MARKDOWNS_ALL: Markdown[] = [
   { id: 6, url: '/src/assets/markdown/commands.md', image: '/src/assets/markdown/images/commands.jpg', title: '开发中常用命令',
     content: '很多命令一段时间没有使用后会忘记的',
     date: '2020.2.28', timeline: 2022},
-  { id: 7, url: '/src/assets/markdown/system_up.md', image: '/src/assets/markdown/images/system_up.jpg', title: '💻 提升各操作系统的技巧',
+  { id: 7, url: '/src/assets/markdown/system_up.md', image: '/src/assets/markdown/images/system_up.jpg', title: '💻 提升各操作系统体验的技巧',
     content: '一些小小的细节可以极大的提升操作系统使用体验',
     date: '2020.3.21', timeline: 2022}
 ];
 
 export const MARKDOWNS_2022: Markdown[] = [
-  { id: 7, url: '/src/assets/markdown/system_up.md', image: '/src/assets/markdown/images/system_up.jpg', title: '💻 提升各操作系统的技巧',
+  { id: 7, url: '/src/assets/markdown/system_up.md', image: '/src/assets/markdown/images/system_up.jpg', title: '💻 提升各操作系统体验的技巧',
     content: '一些小小的细节可以极大的提升操作系统使用体验',
     date: '2020.3.21', timeline: 2022},
   { id: 6, url: '/src/assets/markdown/commands.md', image: '/src/assets/markdown/images/commands.jpg', title: '开发中常用命令',
@@ -58,7 +58,7 @@ export const MARKDOWNS_2021: Markdown[] = [
 ];
 
 export const MARKDOWNS_2020: Markdown[] = [
-  { id: 1, url: '/src/assets/markdown/Uses.md', image: '/src/assets/photos/012.jpg', title: '我使用了哪些工具？',
+  { id: 1, url: '/src/assets/markdown/uses.md', image: '/src/assets/photos/012.jpg', title: '我使用了哪些工具？',
     content: '了解我使用哪些软件和设备',
     date: '2020.8.31', timeline: 2020}
 ];
