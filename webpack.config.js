24a9cacb4bf9138fb3b5794e2438cc0cbbb84edb
shadowmelds <hujincan15369@Gmail.com>
2022-07-25 00:40:33 +0800

    内容更新

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,10 @@ module.exports = {
         home: ['./src/app/ui/home/home.js', './src/app/index.js', './src/app/ui/home/skill-bar.js', './src/app/ui/home/skill-button.js', './src/app/ui/home/project-tiles.js'],
         blog: ['./src/app/ui/blog/blog.js'],
         mdpage: ['./src/app/ui/blog/md-page/md-page.js'],
-        base: ['./src/app/ui/application/base.js', './src/app/ui/application/jump-icons.js'],
+        base: ['./src/app/res/application/base.js', './src/app/res/application/jump-icons.js'],
         photo: ['./src/app/ui/photo/photo.js'],
-        theme: ['./src/app/ui/application/theme.js'],
-        util: ['./src/app/ui/application/http-request-util.js']
+        theme: ['./src/app/res/application/theme.js'],
+        util: ['./src/app/res/application/http-request-util.js']
     },
     output: {
         path: path.resolve(__dirname, "./dist"),
